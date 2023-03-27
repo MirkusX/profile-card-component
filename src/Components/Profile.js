5683d30f5f3c8ac775f1877feb7828935cefd555
MirkusX <marcusk.kodehode@gmail.com>
2023-03-27 12:42:38 +0200

    Made some huge improvements that nobody else could make

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,7 +2,28 @@ export const Profile = () => {
   return (
     <section>
       <div>
-        <h1>Profile</h1>
+        <div />
+        <div>
+          <div>
+            <h1>Victor Crest</h1>
+            <p>26</p>
+          </div>
+          <p>London</p>
+          <div>
+            <div>
+              <p>80K</p>
+              <p>Followers</p>
+            </div>
+            <div>
+              <p>803K</p>
+              <p>Likes</p>
+            </div>
+            <div>
+              <p>1.4K</p>
+              <p>Photos</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
